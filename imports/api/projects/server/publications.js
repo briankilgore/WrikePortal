8e739c5ae9cd5cf75f4658605d56cbe1683da7ab
Brian Kilgore <brian@sugarcrm.com>
2017-02-09 09:19:10 -0800

    Change projects structure  - Get projects from Wrike API instead of having Sugar create projects locally  - Filter projects by sugar_account_id metadata and corresponding account_id in user record.

--- imports/api/projects/server/publications.js
+++ imports/api/projects/server/publications.js
@@ -1,17 +1,67 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Projects } from '../projects.js';
+import { Random } from 'meteor/random';
 
-Meteor.publish('projects.all', function () {
+let baseUrl = "https://www.wrike.com/api/v3";
+
+// Meteor.publish('projects.all', function () {
+//     if(this.userId) {
+//         let user = Meteor.users.findOne(this.userId);
+//         console.log(user);
+
+//         let projects = Projects.find({
+//             accountId: user.accountId
+//         });
+//         // console.log(projects);
+//         return projects;
+//     }
+//     else {
+//         this.ready();
+//     }
+// });
+
+// Meteor.publish('projects.getById', function (projectId) {
+//     if(this.userId) {
+//         let project = Projects.find(projectId);
+//         // console.log(projects);
+//         return project;
+//     }
+//     else {
+//         this.ready();
+//     }
+// });
+
+Meteor.publish('projects.getAll', function () {
     if(this.userId) {
-        let user = Meteor.users.findOne(this.userId);
-        console.log(user);
+        let options = {
+            headers: {
+                "Authorization": "Bearer " + Meteor.settings.private.wrike.access_token
+            }
+        };
 
-        let projects = Projects.find({
-            accountId: user.accountId
-        });
-        // console.log(projects);
-        return projects;
+        try {
+            let user = Meteor.users.findOne(this.userId);
+            //{"key":"sugar_account_id","value":"03b12176-ec02-11e6-b688-9a9ad8099ce9"}
+            let metadata = {
+                key: "sugar_account_id",
+                value: user.accountId
+            };
+
+            let url = baseUrl + "/folders/" + Meteor.settings.private.wrike.customer_folder_id + "/folders?metadata=" + JSON.stringify(metadata);
+            console.log(url);
+            let projects = HTTP.call("GET", url, options);
+
+            _.each(projects.data.data, (project) => {
+                this.added('projects', Random.id(), project);
+            });
+
+            this.ready();
+        }
+        catch(e) {
+            console.log(e);
+            throw new Meteor.Error('error-retrieving', 'Unable to communicate with server');
+        }
     }
     else {
         this.ready();
@@ -19,10 +69,31 @@ Meteor.publish('projects.all', function () {
 });
 
 Meteor.publish('projects.getById', function (projectId) {
-    if(this.userId) {
-        let project = Projects.find(projectId);
-        // console.log(projects);
-        return project;
+    if(this.userId && projectId) {
+        let options = {
+            headers: {
+                "Authorization": "Bearer " + Meteor.settings.private.wrike.access_token
+            }
+        };
+
+        try {
+            let self = this;
+            let pollingFactor = (Meteor.settings.public.env == "development") ? .1 : 1;
+            (function doPoll() {
+                let projects = HTTP.call("GET", baseUrl + "/folders/" + projectId, options);
+
+                _.each(projects.data.data, (project) => {
+                    self.added('projects', project.id, project);
+                });
+
+                self.ready();
+                Meteor.setTimeout(doPoll, 60000 * pollingFactor);
+            })();
+        }
+        catch(e) {
+            console.log(e);
+            throw new Meteor.Error('error-retrieving', 'Unable to communicate with server');
+        }
     }
     else {
         this.ready();
